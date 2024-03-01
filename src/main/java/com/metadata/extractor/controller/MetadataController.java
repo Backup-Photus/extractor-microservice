08a8dde31194812cde337fccc418d33959fc3830
shreyasSarve <i.made.jarvis@gmail.com>
2024-03-01 23:42:58 +0530

    Added get mapping for ping (checking running status of application)

--- src/main/java/com/metadata/extractor/controller/MetadataController.java
+++ src/main/java/com/metadata/extractor/controller/MetadataController.java
@@ -6,10 +6,7 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/metadata")
@@ -21,6 +18,11 @@ public class MetadataController {
     @PostMapping
     private ResponseEntity<HttpStatus> metadata(@Valid @RequestBody LoadingMetadataDTO data){
         service.addEntryForMetadata(data.toLoadingMetadata());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping
+    private ResponseEntity<HttpStatus> ping(){
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
